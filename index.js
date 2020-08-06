74eb57c00e9ea7e2daa2ef4b25349ad17130f35c
Diego Chapedelaine <diegochappedelaine@users.noreply.github.com>
2020-08-06 15:22:04 +0200

    second commit

--- index.js
+++ index.js
@@ -8,18 +8,23 @@ app.use(cors());
 
 require("dotenv").config();
 
+// Accès à l'API
+const timestamp = Date.now();
+const myHash = MD5(
+    timestamp +
+        process.env.MARVEL_PRIVATE_API_KEY +
+        process.env.MARVEL_PUBLIC_API_KEY
+);
+
 app.get("/comics", async (req, res) => {
     console.log("route /comics");
     const offset = req.query.offset;
-
-    const timestamp = Date.now();
-    const myHash = MD5(
-        timestamp +
-            process.env.MARVEL_PRIVATE_API_KEY +
-            process.env.MARVEL_PUBLIC_API_KEY
-    );
+    const titleStartsWith = req.query.titleStartsWith;
 
     let apiLink = "http://gateway.marvel.com/v1/public/comics?limit=100";
+    if (titleStartsWith) {
+        apiLink += `&titleStartsWith=${titleStartsWith}`;
+    }
     if (offset) {
         apiLink += `&offset=${offset}`;
     }
@@ -33,16 +38,26 @@ app.get("/comics", async (req, res) => {
     }
 });
 
+app.get("/comicssSearch", async (req, res) => {
+    console.log("route /comicssSearch");
+
+    const titleStartsWith = req.query.titleStartsWith;
+
+    let apiLink = "http://gateway.marvel.com/v1/public/comics?limit=100";
+    if (titleStartsWith) {
+        apiLink = `http://gateway.marvel.com/v1/public/comics?titleStartsWith=${titleStartsWith}`;
+    }
+    apiLink += `&limit=100&ts=${timestamp}&apikey=${process.env.MARVEL_PUBLIC_API_KEY}&hash=${myHash}`;
+    try {
+        const response = await axios.get(apiLink);
+        res.json(response.data.data);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 app.get("/characters", async (req, res) => {
     console.log("route /characters");
-
-    // Accès à l'API
-    const timestamp = Date.now();
-    const myHash = MD5(
-        timestamp +
-            process.env.MARVEL_PRIVATE_API_KEY +
-            process.env.MARVEL_PUBLIC_API_KEY
-    );
 
     const offset = req.query.offset;
     const nameStartsWith = req.query.nameStartsWith;
@@ -57,6 +72,24 @@ app.get("/characters", async (req, res) => {
 
     apiLink += `&ts=${timestamp}&apikey=${process.env.MARVEL_PUBLIC_API_KEY}&hash=${myHash}`;
 
+    try {
+        const response = await axios.get(apiLink);
+        res.json(response.data.data);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get("/charactersSearch", async (req, res) => {
+    console.log("route /charactersSearch");
+
+    const nameStartsWith = req.query.nameStartsWith;
+
+    let apiLink = "http://gateway.marvel.com/v1/public/characters?limit=100";
+    if (nameStartsWith) {
+        apiLink = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameStartsWith}`;
+    }
+    apiLink += `&limit=100&ts=${timestamp}&apikey=${process.env.MARVEL_PUBLIC_API_KEY}&hash=${myHash}`;
     try {
         const response = await axios.get(apiLink);
         res.json(response.data.data);
